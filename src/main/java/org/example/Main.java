54dac04cd90beac1a7f71e7ab3e3343087f1b7b6
ZacharyARoss <zachross10@gmail.com>
2023-05-24 11:33:50 -0400

    or example

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -15,12 +15,15 @@ public class Main {
         if (topScore > 100) {
             System.out.println("You got the high score!");
         }
-
+//&& example
         int secondTopScore = 60;
-        if (topScore > secondTopScore && topScore < 100){
+        if ((topScore > secondTopScore) && (topScore < 100)){
             System.out.println("Greater than second top score and less than 100");
         }
 
-
+// or || example
+        if((topScore < 90) || (secondTopScore <= 90)){
+            System.out.println("either or both of the conditions are true");
+        }
     }
 }
