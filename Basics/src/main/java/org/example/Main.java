1947108e518427f182358800646ba620439d20b6
ZacharyARoss <125410959+ZacharyARoss@users.noreply.github.com>
2023-05-23 22:15:48 -0400

    Add files via upload

---
+++ Basics/src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+//print line
+        System.out.printf("Hello my name is Zach!");
+
+//basic if statement
+        boolean isAlien = false;
+        if (isAlien == false){
+            System.out.println("That's not an Alien!");
+    }
+
+
+    }
+}
